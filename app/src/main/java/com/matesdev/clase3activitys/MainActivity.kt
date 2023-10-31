54a460a1f7649d68f9a6537f9f0323a9d3c125cd
guillermogodoypro <guillermo.rgo92@gmail.com>
2023-10-30 22:07:03 -0300

    app finalizada

--- app/src/main/java/com/matesdev/clase3activitys/MainActivity.kt
+++ app/src/main/java/com/matesdev/clase3activitys/MainActivity.kt
@@ -7,15 +7,21 @@ import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     private lateinit var btContinuar: Button
-
+    private lateinit var nombre: String
+    private lateinit var edad: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btContinuar = findViewById(R.id.btirasecond)
+        nombre = "Aspros"
+        edad = "55"
 
         btContinuar.setOnClickListener {
             val intent = Intent(  this, MainActivity2::class.java )
+            intent.putExtra("name", nombre)
+            intent.putExtra("age", edad)
+
             startActivity(intent)
         }
     }
--- app/src/main/java/com/matesdev/clase3activitys/MainActivity2.kt
+++ app/src/main/java/com/matesdev/clase3activitys/MainActivity2.kt
@@ -4,18 +4,32 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity2 : AppCompatActivity() {
     private lateinit var btiramain: Button
-
+    private lateinit var setNombre: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
+        val bundle = intent.extras
+
+
         btiramain = findViewById(R.id.btgotomain)
+
+        val nombre = bundle?.getString("name", "")
+        val edad = bundle?.getString("age", "")
+
+        setNombre = findViewById(R.id.tvNombre)
+
+        Toast.makeText(this, "$nombre $edad", Toast.LENGTH_SHORT).show()
+        setNombre.text = nombre
 
         btiramain.setOnClickListener {
             val intent = Intent( this, MainActivity::class.java  )
+
 
             startActivity(intent)
         }
